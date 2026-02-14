9c8f5be24eed7f1ece4f752fbd6bb20767aac8f2
DeveloperAmrit <kittyamrit005@gmail.com>
2026-02-14 21:26:00 +0530

    Add proper folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
+import Hero from "@/components/Home/Hero";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-16">
-      <main className="flex flex-col gap-8 items-center text-center">
-        <h1 className="text-4xl font-bold">Hello, OrgExplorer!</h1>
-      </main>
-    </div>
+    <Hero/>
   );
 }
---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>Hello OrgExplorer</div>
+  )
+}
+
+export default Hero
