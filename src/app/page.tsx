baea4a56284477ab44d528c124cb7d93243ab7f5
DeveloperAmrit <kittyamrit005@gmail.com>
2026-02-14 21:09:08 +0530

    Migrated to next and tailwind latest version

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-16">
+      <main className="flex flex-col gap-8 items-center text-center">
+        <h1 className="text-4xl font-bold">Hello, OrgExplorer!</h1>
+      </main>
+    </div>
+  );
+}
